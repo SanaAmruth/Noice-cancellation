02a3d4f78841bfa18a9da875645cb0bf2c659a1d
AMRUTH HARSHA <60221104+SanaAmruth@users.noreply.github.com>
2021-02-14 12:30:52 +0530

    Update stat.m

--- stat.m
+++ stat.m
@@ -2,8 +2,6 @@ function [mean,var,skew,kurto]=stat(x)
     mean=0;
     var=0;
     skew=0;
-    mu3=0;
-    mu4=0;
     kurto=0;
     n=length(x);
     for i=1:n
@@ -12,12 +10,10 @@ function [mean,var,skew,kurto]=stat(x)
     mean=mean/n;
     for i=1:n
         var=var+(mean-x(i)*(mean-x(i)));
-        mu3=mu3+(mean-x(i)*(mean-x(i))*(mean-x(i)));
-        mu4=mu4+(mean-x(i)*(mean-x(i))*(mean-x(i))*(mean-x(i)));
+        skew=skew+(mean-x(i)*(mean-x(i))*(mean-x(i)));
+        kurto=kurto+(mean-x(i)*(mean-x(i))*(mean-x(i))*(mean-x(i)));
     end
     var=var/n;
-    mu3=m3/n;
-    mu4=mu4/n;
-    skew=(mu3*mu3)/(var*var);
-    kurto=mu4/var;
+    skew=skew/n;
+    kurto=kurto/n;
 end
